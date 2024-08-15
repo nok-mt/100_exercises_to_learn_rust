60ace75f2ba604c9990febd405984d38e4e6f4b7
nok-mt <79310823+nok-mt@users.noreply.github.com>
2024-08-15 10:48:58 +0900

    Passed:02_basic_calculator/07_for

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,15 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    if n == 0 {
+        1
+    } else {
+        let mut result = 1;
+        for i in 1..(n + 1) {
+            result = result * i
+        }
+
+        result
+    }
 }
 
 #[cfg(test)]
