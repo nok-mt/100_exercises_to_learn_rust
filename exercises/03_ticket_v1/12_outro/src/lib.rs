f8b2b53e819618e1bf68d72d3f46f15ad3f9a8ec
nok-mt <79310823+nok-mt@users.noreply.github.com>
2024-08-23 16:47:35 +0900

    Passed:03_ticket_v1/12_outro

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,77 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        Self::validate_product_name(&product_name);
+        Self::validate_quantity(&quantity);
+        Self::validate_unit_price(&unit_price);
+
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    // validation
+    fn validate_product_name(product_name: &str) -> () {
+        if product_name.is_empty() {
+            panic!("Product name can't be empty.")
+        } else if product_name.len() > 300 {
+            panic!("Product name can't be longer than 300 bytes.")
+        }
+    }
+
+    fn validate_quantity(quantity: &u32) -> () {
+        if quantity.eq(&0) {
+            panic!("Quantity must be strictly greater than zero.")
+        }
+    }
+
+    fn validate_unit_price(unit_price: &u32) -> () {
+        if unit_price.eq(&0) {
+            panic!("Unit price is in cents and must be strictly greater than zero.")
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        &self.unit_price * &self.quantity
+    }
+
+    // setters
+    pub fn set_product_name(self: &mut Order, new_product_name: String) -> () {
+        Self::validate_product_name(&new_product_name);
+        self.product_name = new_product_name.to_string()
+    }
+
+    pub fn set_quantity(self: &mut Order, new_quantity: u32) -> () {
+        Self::validate_quantity(&new_quantity);
+        self.quantity = new_quantity
+    }
+
+    pub fn set_unit_price(self: &mut Order, new_unit_price: u32) -> () {
+        Self::validate_unit_price(&new_unit_price);
+        self.unit_price = new_unit_price
+    }
+
+    // getters
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+}
